b3b915c392e786db8482fb221c96a2b8ce2ed403
Dzedas ingmanLT <ddzedas@gmail.com>
2023-12-26 17:27:46 +0100

    project added

---
+++ CMD_Upgrade/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CMD_Upgrade
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void HandleMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse ellipse)
+            {
+                Color color = (Color)ColorConverter.ConvertFromString("#F2AC61");
+                ellipse.Fill = new SolidColorBrush(color);
+                ellipse.Cursor = Cursors.Hand;
+            }
+        }
+
+        private void HandleMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse ellipse)
+            {
+                Color color = (Color)ColorConverter.ConvertFromString("#E81010");
+                ellipse.Fill = new SolidColorBrush(color);
+            }
+        }
+    }
+
+
+}
---
+++ CMD_Upgrade/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CMD_Upgrade
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Rectangle rectangle)
+            {
+                Color color = (Color)ColorConverter.ConvertFromString("#FF4040");
+                rectangle.Fill = new SolidColorBrush(color);
+                rectangle.Cursor = Cursors.Hand;
+            }
+        }
+
+        private void HandleMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Rectangle rectangle)
+            {
+                Color color = (Color)ColorConverter.ConvertFromString("#000000");
+                rectangle.Fill = new SolidColorBrush(color);
+            }
+        }
+    }
+}
